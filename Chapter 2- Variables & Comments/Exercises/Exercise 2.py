6664b67d117e1b13feeb42c8c186561a9f2425d4
saif-055 <saifsafi199@gmail.com>
2023-11-18 19:00:41 +0400

    chapter 2
    i have finished chapter 2

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 2.py
@@ -0,0 +1,8 @@
+# Define the quote and its author
+quote = "Peace cannot be kept by force; it can only be achieved by understanding."
+author = "Albert Einstein"
+
+# Print the quote and its author
+print(f'{author} once said, "{quote}"')
+
+print('never tried anything new."')
---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 5.py
@@ -0,0 +1,4 @@
+a=60
+b=7
+print(int(a/b))
+print(a%b)

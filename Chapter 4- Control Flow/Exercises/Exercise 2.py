fb5144e9ead33787807873f3bc4cc247cb3330f8
saif-055 <saifsafi199@gmail.com>
2023-11-18 19:02:11 +0400

    chapter 4
    i have finished chapter 4

---
+++ Chapter 4- Control Flow/Exercises/Exercise 2.py
@@ -0,0 +1,12 @@
+alien_color = 'green'
+if alien_color == 'green':
+   print('you earned 5 points for killing the alein')
+else:
+   print('you earned 10 points')
+   
+   
+   alien_color == 'red'
+   if alien_color == 'green':
+      print('you earned 5 points for killing the alein')
+   else:
+      print('you earned 10 points')
